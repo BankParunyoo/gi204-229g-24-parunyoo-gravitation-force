630dad0e10afa73c5b661d7b0c2b9040b4ff0f15
BankParunyoo <parunyoo.nijj@bumail.net>
2024-03-15 10:21:31 +0700

    Gravitation Force

---
+++ Assets/PlanetAttractor.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetAttractor : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    private const float G = 6.67f;
+
+    public static List< PlanetAttractor > pAttractors;
+
+    void AttractorFormula(PlanetAttractor other)
+    {
+        Rigidbody rbOther = other.rb;
+
+        Vector3 direction = rb.position - rbOther.position;
+
+        float distance = direction.magnitude;
+
+        //F = G * (m1 * m2)/d^2;
+        float forceMagnitude = G * ( rb.mass * rbOther.mass ) / Mathf.Pow( distance, 2 );
+
+        Vector3 forceDir = direction.normalized * forceMagnitude;
+
+        rbOther.AddForce(forceDir);
+
+    }//AttractorFormula
+
+    void FixedUpdate()
+    {
+        foreach ( var attractor in pAttractors )
+        {
+            if( attractor != this )
+            {
+                AttractorFormula ( attractor );
+            }
+        }
+
+    }//FixedUpdate
+
+    private void OnEnable()
+    {
+        if (pAttractors == null)
+        {
+            pAttractors = new List< PlanetAttractor >();
+        }
+        pAttractors.Add( this );
+    }//OnEnable
+
+}//PlanetAttractor
